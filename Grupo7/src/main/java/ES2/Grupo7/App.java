5dc062c846413ba56fcd6314f25714e397341fef
tcnma <tcnma@iscte-iul.pt>
2020-03-19 22:46:15 +0000

    Criação de uma função que calcula uma potencia.

--- Grupo7/src/main/java/ES2/Grupo7/App.java
+++ Grupo7/src/main/java/ES2/Grupo7/App.java
@@ -4,10 +4,29 @@ package ES2.Grupo7;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+	
+	public int potencia (int base, int expoente) {
+		int res = (int)(Math.pow(base, expoente));
+		return res;
+	}
+	
+    public static void main( String[] args ){
+    	
+    	int base = 2;
+    	int expoente = 2;
+    	App p = new App();
+    	int res = p.potencia(base, expoente);
+    	System.out.println("Resultado da potencia de " + base + "^" + expoente + " = " + res + "\n");
+    	
+        System.out.println( "Trabalho desenvolvido por: " + "\n" + "\n" 
+        + "Tomás Afonso" + "\n" 
+        + "Diogo Guerreiro" + "\n" 
+        + "Paulo Jin" + "\n" 
+        + "Zé Magalhães" + "\n" 
+        + "André Ivan-Coca" + "\n" 
+        + "António Oliveira");
     }
+    
+    
 }
