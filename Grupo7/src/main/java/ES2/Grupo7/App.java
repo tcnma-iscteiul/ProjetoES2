acb0240b83a691099cb096597b81d946f94b650f
amboa-iscteiul <amboa@iscte-iul.pt>
2020-03-26 16:47:59 +0000

    JavaDoc implemented

--- Grupo7/src/main/java/ES2/Grupo7/App.java
+++ Grupo7/src/main/java/ES2/Grupo7/App.java
@@ -1,20 +1,36 @@
 package ES2.Grupo7;
 
 /**
- * Hello world!
+ * @author ES2-IGEPL-Grupo7
  *
  */
 public class App{
 	
+	
+	/**
+	 * função que, ao ser inserido 2 inteiros, devolve a potência calculada conforme a base e o expoente
+	 * @param base
+	 * @param expoente
+	 * @return a potencia calculada
+	 *  **/
 	public int potencia (int base, int expoente) {
 		int res = (int)(Math.pow(base, expoente));
 		return res;
 	}
 	
+	
+	/**
+	 * função que compara dois valores do tipo String e pode devolver uma entre 2 hipóteses: 
+	 * true = as Strings são iguais
+	 * false = as Strings são diferentes
+	 * @param String g
+	 * @param String w
+	 * @return um booleano
+	 *  **/
 	public boolean compare (String g, String w) {
 		return g.contains(w);
 	}
-	
+
     public static void main( String[] args ){
     	
     	int base = 2;
